851b8fa8dfe7b11602e56b228c8ddb5850183c62
Koichi <kohichi.nishide@nts.ricoh.co.jp>
2013-02-04 13:49:02 +0900

    branch test

--- AndroidSampleApp/src/com/example/androidsampleapp/MainActivity.java
+++ AndroidSampleApp/src/com/example/androidsampleapp/MainActivity.java
@@ -12,9 +12,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+  
+    
+    
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	// test
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
